a3371343150d9fefddb2baa197c91b4e8ed34c67
Pierre Haller <pierre.haller@datalegaldrive.com>
2022-01-31 17:45:24 +0100

    init game

---
+++ src/lib/Grid.svelte
@@ -0,0 +1,136 @@
+<script lang="ts">
+	import { spring } from 'svelte/motion';
+	type CellIndex = `${number}:${number}`;
+
+	let height: number = 0;
+	let width: number = 0;
+	let offset = 2;
+	let rectSize = 50;
+	let interval: NodeJS.Timer;
+	export let playing = false;
+
+	export let alivedCells: CellIndex[] = [];
+
+	$: xRectsCounts = Math.floor(width / rectSize) + 1 ?? 0;
+	$: yRectsCounts = Math.floor(height / rectSize) + 1 ?? 0;
+
+	const smoothRectSize = spring(rectSize);
+	$: smoothRectSize.set(rectSize);
+
+	function getNeighboors(coords: { x: number; y: number }) {
+		const neighboors: CellIndex[] = [];
+		for (let x = -1; x <= 1; x++) {
+			for (let y = -1; y <= 1; y++) {
+				const neighboorX = x + coords.x;
+				const neighboorY = y + coords.y;
+				if (
+					neighboorX < 0 ||
+					neighboorY < 0 ||
+					neighboorX > xRectsCounts ||
+					neighboorY > yRectsCounts ||
+					(neighboorX === coords.x && neighboorY === coords.y)
+				) {
+					continue;
+				}
+				neighboors.push(`${neighboorX}:${neighboorY}`);
+			}
+		}
+		return neighboors;
+	}
+
+	function giveLife(x: number, y: number) {
+		const key = `${x}:${y}` as const;
+		if (alivedCells.includes(key)) {
+			alivedCells = alivedCells.filter((curKey) => curKey !== key);
+		} else {
+			alivedCells = [...alivedCells, key];
+		}
+	}
+
+	function onScroll(e: WheelEvent) {
+		if (e.deltaY < 0) {
+			rectSize += 2;
+		}
+		if (e.deltaY > 0 && rectSize > 20) {
+			rectSize -= 2;
+		}
+	}
+
+	function nextTick() {
+		const newAlive: CellIndex[] = [];
+		for (let x = 0; x < xRectsCounts; x++) {
+			for (let y = 0; y < yRectsCounts; y++) {
+				const isAlive = alivedCells.includes(`${x}:${y}`);
+				const neighboors = getNeighboors({ x, y });
+				const alivedNeighboors = neighboors.filter((neighboor) =>
+					alivedCells.includes(neighboor)
+				).length;
+				console.log({ x, y, isAlive, neighboors });
+
+				if (isAlive && (alivedNeighboors === 2 || alivedNeighboors === 3)) {
+					newAlive.push(`${x}:${y}`);
+				} else if (alivedNeighboors === 3) {
+					newAlive.push(`${x}:${y}`);
+				}
+			}
+		}
+		alivedCells = newAlive;
+	}
+
+	function play() {
+		playing = true;
+		interval = setInterval(nextTick, 200);
+	}
+
+	function stop() {
+		playing = false;
+		clearInterval(interval);
+	}
+</script>
+
+<svelte:window on:wheel={onScroll} />
+
+<div class="grid" bind:clientHeight={height} bind:clientWidth={width}>
+	<button class="reset" on:click={() => (alivedCells = [])}>Clear</button>
+	<button class="start" on:click={playing ? stop : play}>{playing ? 'Stop' : 'Start'}</button>
+	<svg width="100%" height="100%">
+		{#each Array(xRectsCounts) as _, x}
+			{#each Array(yRectsCounts) as _, y}
+				<rect
+					class={alivedCells.includes(`${x}:${y}`) ? 'selected' : 'rect'}
+					x={x * $smoothRectSize}
+					y={y * $smoothRectSize}
+					width={$smoothRectSize - offset}
+					height={$smoothRectSize - offset}
+					on:click={() => giveLife(x, y)}
+				/>
+			{/each}
+		{/each}
+	</svg>
+</div>
+
+<style>
+	.grid {
+		width: 100%;
+		height: 100%;
+	}
+	.reset {
+		position: fixed;
+	}
+	.start {
+		position: fixed;
+		right: 0;
+	}
+	.rect {
+		pointer-events: all;
+	}
+	.rect:hover {
+		fill: #800080;
+	}
+	.selected {
+		fill: #800080;
+	}
+	.selected:hover {
+		fill: black;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import '@fontsource/roboto';
+	import Grid from '$lib/Grid.svelte';
+</script>
+
+<svelte:head>
+	<title>Game of Life</title>
+</svelte:head>
+
+<Grid />
+
+<style>
+	:global(body, html, #svelte) {
+		font-family: 'Roboto';
+		width: 100vw;
+		height: 100vh;
+		background-color: #888888;
+		margin: 0;
+	}
+	h1 {
+		width: 100%;
+		text-align: center;
+	}
+</style>
