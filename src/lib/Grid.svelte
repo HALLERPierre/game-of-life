c769d69a2c8cbebbc01230bf5279959a5c242f60
Pierre Haller <pierre.haller@datalegaldrive.com>
2022-02-01 01:07:07 +0100

    improvements

--- src/lib/Grid.svelte
+++ src/lib/Grid.svelte
@@ -2,34 +2,38 @@
 	import { spring } from 'svelte/motion';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 
 	type CellIndex = `${number}:${number}`;
 
 	let urlPattern: string | undefined | null;
 	let urlZoom: string | undefined | null;
 	onMount(() => {
-		urlPattern = $page.url.searchParams.get('pattern');
-		urlZoom = $page.url.searchParams.get('zoom');
+		// use hash for gh-pages
+		const searchParams = new URLSearchParams($page.url.hash.replace('#', ''));
+		urlPattern = searchParams.get('pattern');
+		urlZoom = searchParams.get('zoom');
 	});
 
 	let height: number = 0;
 	let width: number = 0;
-	let offset = 2;
+	let offset = 0;
 	let rectSize = 50;
 	let interval: NodeJS.Timer;
-	export let playing = false;
+	let speed = 200;
 
-	export let alivedCells: CellIndex[] = [];
+	export let playing = false;
+	export let aliveCells: CellIndex[] = [];
 
 	$: xRectsCounts = Math.floor(width / rectSize) + 1 ?? 0;
 	$: yRectsCounts = Math.floor(height / rectSize) + 1 ?? 0;
 
-	const smoothRectSize = spring(rectSize);
+	const smoothRectSize = spring(rectSize, { damping: 1, precision: 1, stiffness: 1 });
 	$: smoothRectSize.set(rectSize);
 
 	$: {
 		if (urlPattern !== undefined && urlPattern !== null) {
-			alivedCells = JSON.parse(decodeURI(urlPattern));
+			aliveCells = JSON.parse(decodeURI(urlPattern));
 		}
 		if (urlZoom !== undefined && urlZoom !== null) {
 			rectSize = JSON.parse(decodeURI(urlZoom));
@@ -42,13 +46,7 @@
 			for (let y = -1; y <= 1; y++) {
 				const neighboorX = x + coords.x;
 				const neighboorY = y + coords.y;
-				if (
-					neighboorX < 0 ||
-					neighboorY < 0 ||
-					neighboorX > xRectsCounts ||
-					neighboorY > yRectsCounts ||
-					(neighboorX === coords.x && neighboorY === coords.y)
-				) {
+				if (neighboorX === coords.x && neighboorY === coords.y) {
 					continue;
 				}
 				neighboors.push(`${neighboorX}:${neighboorY}`);
@@ -58,11 +56,12 @@
 	}
 
 	function giveLife(x: number, y: number) {
+		stop();
 		const key = `${x}:${y}` as const;
-		if (alivedCells.includes(key)) {
-			alivedCells = alivedCells.filter((curKey) => curKey !== key);
+		if (aliveCells.includes(key)) {
+			aliveCells = aliveCells.filter((curKey) => curKey !== key);
 		} else {
-			alivedCells = [...alivedCells, key];
+			aliveCells = [...aliveCells, key];
 		}
 	}
 
@@ -70,40 +69,80 @@
 		if (e.deltaY < 0) {
 			rectSize += 2;
 		}
-		if (e.deltaY > 0 && rectSize > 20) {
+		if (e.deltaY > 0 && rectSize >= 10) {
 			rectSize -= 2;
 		}
+		console.log(rectSize);
 	}
 
 	function nextTick() {
 		const newAlive: CellIndex[] = [];
-		for (let x = 0; x < xRectsCounts; x++) {
-			for (let y = 0; y < yRectsCounts; y++) {
-				const isAlive = alivedCells.includes(`${x}:${y}`);
+
+		const [minX, maxX, minY, maxY] = aliveCells.reduce(
+			([minX, maxX, minY, maxY], curIndex) => {
+				const [x, y] = curIndex.split(':').map((number) => parseInt(number, 10));
+				return [Math.min(minX, x), Math.max(maxX, x), Math.min(minY, y), Math.max(maxY, y)];
+			},
+			[+Infinity, -Infinity, +Infinity, -Infinity]
+		);
+
+		for (let x = minX - 1; x <= maxX + 1; x++) {
+			for (let y = minY - 1; y <= maxY + 1; y++) {
+				const key = `${x}:${y}` as const;
+				const isAlive = aliveCells.includes(key);
 				const neighboors = getNeighboors({ x, y });
 				const alivedNeighboors = neighboors.filter((neighboor) =>
-					alivedCells.includes(neighboor)
+					aliveCells.includes(neighboor)
 				).length;
-				console.log({ x, y, isAlive, neighboors });
 
 				if (isAlive && (alivedNeighboors === 2 || alivedNeighboors === 3)) {
-					newAlive.push(`${x}:${y}`);
+					newAlive.push(key);
 				} else if (alivedNeighboors === 3) {
-					newAlive.push(`${x}:${y}`);
+					newAlive.push(key);
 				}
 			}
 		}
-		alivedCells = newAlive;
+		aliveCells = newAlive;
 	}
 
 	function play() {
 		playing = true;
-		interval = setInterval(nextTick, 200);
+		interval = setInterval(nextTick, speed);
 	}
 
 	function stop() {
 		playing = false;
 		clearInterval(interval);
+	}
+
+	function faster() {
+		if (speed <= 20) {
+			return;
+		}
+		speed -= 20;
+		clearInterval(interval);
+		interval = setInterval(nextTick, speed);
+	}
+
+	function slower() {
+		speed += 20;
+		clearInterval(interval);
+		interval = setInterval(nextTick, speed);
+	}
+
+	function copyPattern() {
+		const params = new URLSearchParams('');
+
+		params.set('pattern', encodeURI(JSON.stringify(aliveCells)));
+		params.set('zoom', rectSize.toString());
+
+		goto(`#?${params.toString()}`, {
+			keepfocus: true,
+			replaceState: true,
+			noscroll: true
+		});
+
+		window.navigator.clipboard.writeText(window.location.href);
 	}
 </script>
 
@@ -111,28 +150,28 @@
 
 <div class="grid" bind:clientHeight={height} bind:clientWidth={width}>
 	<div class="actions">
-		<button class="reset" on:click={() => (alivedCells = [])}>Clear</button>
+		<button class="reset" on:click={() => (aliveCells = [])}>Clear</button>
 		<button class="start" on:click={playing ? stop : play}>{playing ? 'Stop' : 'Start'}</button>
-		<button
-			class="start"
-			on:click={() =>
-				navigator.clipboard.writeText(
-					`${window.location.origin}?pattern=${encodeURI(
-						JSON.stringify(alivedCells)
-					)}&zoom=${$smoothRectSize}`
-				)}>Copy</button
-		>
+		<button class="start" on:click={copyPattern}>Copy</button>
+		<div class="speed">
+			<button on:click={slower}>-</button>
+			<button on:click={faster}>+</button>
+		</div>
 	</div>
+	<span class="counter">{speed / 1000} / s</span>
 	<svg width="100%" height="100%">
 		{#each Array(xRectsCounts) as _, x}
 			{#each Array(yRectsCounts) as _, y}
 				<rect
-					class={alivedCells.includes(`${x}:${y}`) ? 'selected' : 'rect'}
+					class="rect"
+					class:selected={aliveCells.includes(`${x}:${y}`)}
 					x={x * $smoothRectSize}
 					y={y * $smoothRectSize}
 					width={$smoothRectSize - offset}
 					height={$smoothRectSize - offset}
 					on:click={() => giveLife(x, y)}
+					rx={Math.log($smoothRectSize)}
+					ry={Math.log($smoothRectSize)}
 				/>
 			{/each}
 		{/each}
@@ -160,21 +199,33 @@
 	button {
 		text-transform: uppercase;
 		font-size: 2rem;
-		padding: 25px 40px;
+		padding: 15px 40px;
 		background-color: green;
 		border: none;
 		cursor: pointer;
 	}
+
+	.speed {
+		display: flex;
+		flex-direction: column;
+		gap: 1px;
+	}
+
+	.counter {
+		position: fixed;
+		top: 95%;
+		left: 1%;
+		color: green;
+		text-align: center;
+	}
 	.rect {
 		pointer-events: all;
+		transition: fill 0.1s ease;
 	}
 	.rect:hover {
-		fill: #800080;
+		fill-opacity: 50%;
 	}
 	.selected {
 		fill: #800080;
-	}
-	.selected:hover {
-		fill: black;
 	}
 </style>
